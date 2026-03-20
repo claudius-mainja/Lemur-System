849d6dc7676c193319d14bdf7114223af6658401
Claudisu Mainja <claudius.mainja@gmail.com>
2026-03-20 22:34:09 +0200

    refactor: Remove duplicate backend, update frontend API

--- backend/documents/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Document
-
-
-class DocumentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Document
-        fields = '__all__'
-        read_only_fields = ['id', 'created_at', 'updated_at']
--- backend/productivity/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class ProductivityConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'productivity'
-    verbose_name = 'Productivity'
